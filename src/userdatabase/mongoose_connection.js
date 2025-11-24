6e82a2948dcf76033412233ddb6457e85b92bd6e
yajatdeshpande1513 <yajatdeshpande1513@gmail.com>
2025-11-25 00:56:26 +0530

    Userdatabase

---
+++ src/userdatabase/mongoose_connection.js
@@ -0,0 +1,5 @@
+const mongoose=require("mongoose");
+const users_collection1=require('./userdata')
+mongoose.connect('mongodb://localhost:27017/facebookuserdata')
+.then(()=>{console.log('Mongoose connection successful at port 27017')})
+.catch((err)=>{console.log(err)})
